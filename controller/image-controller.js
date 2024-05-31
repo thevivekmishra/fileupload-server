780d899547eaa7bc4c13890cea6aedf391d0adf8
thevivekmishra <vivekmishra89994@gmail.com>
2024-05-31 22:09:16 +0530

    fileupload-server

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -11,7 +11,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `${process.env.BASE_URL}/${file._id}`});
+        response.status(200).json({ path: `${process.env.BASE_URL}file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
